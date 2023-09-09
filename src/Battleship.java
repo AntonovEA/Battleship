c473fc639874b000245a180b74880bb0ab46375a
AntonovEA <egorartemovich@mail.ru>
2023-09-09 12:48:29 +0500

    player1, 2 + scanner

---
+++ src/Battleship.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Battleship {
+    static String player1;
+    static String player2;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Player 1 plz enter your name:");
+        player1 = scanner.nextLine();
+        System.out.println("Player 1 plz enter your name:");
+        player2 = scanner.nextLine();
+
+
+    }
+    public static void draw(){
+
+
+    }
+}
