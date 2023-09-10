13c1edd850b7a598e2643249b5820013e8f5679d
AntonovEA <egorartemovich@mail.ru>
2023-09-10 21:01:22 +0500

    player1, 2 + scanner + array + ship 1/2

--- src/Battleship.java
+++ src/Battleship.java
@@ -15,13 +15,13 @@ public class Battleship {
     public static void drawField() {
 
         int deck = 4;
-        while (){
-        System.out.println();
-        System.out.println(player1 + "plz place your 4-deck ship on the battlefield");
-        System.out.println("  0 1 2 3 4 5 6 7 8 9");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + " - - - - - - - - - -");
-        }
+        while (deck >= 1) {
+            System.out.println();
+            System.out.println(player1 + "plz place your " + deck +"-deck ship on the battlefield");
+            System.out.println("  0 1 2 3 4 5 6 7 8 9");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " - - - - - - - - - -");
+            }
             System.out.println("Plz enter OX coordinate: ");
             int x = scanner.nextInt();
             System.out.println("Plz enter OY coordinate: ");
@@ -30,15 +30,13 @@ public class Battleship {
             System.out.println("1. Vertical");
             System.out.println("2. Horizontal");
             int direction = scanner.nextInt();
-            battlefield1[x][y] = 1;
-            if (direction == 1) {
-                battlefield1[x][y + 1] = 1;
-                battlefield1[x][y + 2] = 1;
-                battlefield1[x][y + 3] = 1;
-            } else {
-                battlefield1[x + 1][y] = 1;
-                battlefield1[x + 2][y] = 1;
-                battlefield1[x + 3][y] = 1;
+
+            for (int i = 0; i < deck ; i++) {
+                if (direction == 1 ){
+                    battlefield1[x][y+i] =1 ;
+                }else battlefield1[x+i][y]=1;
+            }
+
             }
             for (int i = 0; i < battlefield1.length; i++) {
                 for (int j = 0; j < battlefield1.length; j++) {
@@ -48,4 +46,4 @@ public class Battleship {
         }
 
 
-}
+    }
