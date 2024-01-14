028a01d8c2aa6dbf449e4cc7afcb711f73b58cfb
AntonovEA <egorartemovich@mail.ru>
2024-01-14 21:53:36 +0500

    player1, 2 + scanner + array + ship + moves+ winCondition + Available+

--- src/Battleship.java
+++ src/Battleship.java
@@ -10,11 +10,22 @@ public class Battleship {
     static int[][] monitor2 = new int[10][10];
 
     public static void main(String[] args) {
+        System.out.println("Player 1 plz enter your name");
+        playerName1 = scanner.nextLine();
+        System.out.println("Player 2 plz enter your name");
+        playerName2 = scanner.nextLine();
         placeShips(playerName1, battlefield1);
         placeShips(playerName2, battlefield2);
-        makeTurn(playerName1, monitor1);
-        makeTurn(playerName2, monitor2);
-
+        while (true) {
+            makeTurn(playerName1, monitor1, battlefield2);
+            if (isWinCondition()) {
+                break;
+            }
+            makeTurn(playerName2, monitor2, battlefield1);
+            if (isWinCondition()) {
+                break;
+            }
+        }
     }
 
     public static void placeShips(String playerName, int[][] battlefield) {
@@ -33,6 +44,10 @@ public class Battleship {
             System.out.println("1. Vertical");
             System.out.println("2. Horizontal");
             int direction = scanner.nextInt();
+            if (!isAvailable(x,y,deck,direction,battlefield)){
+                System.out.println("Wrong coordinates");
+                continue;
+            }
 
             for (int i = 0; i < deck; i++) {
                 if (direction == 1) {
@@ -63,33 +78,112 @@ public class Battleship {
     }
 
     public static void makeTurn(String playerName, int[][] monitor, int[][] battlefield) {
-        System.out.println(playerName + ", make your turn.");
-        System.out.println("  0 1 2 3 4 5 6 7 8 9");
-        for (int i = 0; i < monitor.length; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < monitor[1].length; j++) {
-                if (monitor[j][i] == 0) {
-                    System.out.print("- ");
-                } else if (monitor[j][i] == 1) {
-                    System.out.print(". ");
-                } else {
-                    System.out.print("X ");
+        while (true) {
+            System.out.println(playerName + ", make your turn.");
+            System.out.println("  0 1 2 3 4 5 6 7 8 9");
+            for (int i = 0; i < monitor.length; i++) {
+                System.out.print(i + " ");
+                for (int j = 0; j < monitor[1].length; j++) {
+                    if (monitor[j][i] == 0) {
+                        System.out.print("- ");
+                    } else if (monitor[j][i] == 1) {
+                        System.out.print(". ");
+                    } else {
+                        System.out.print("X ");
+                    }
+
                 }
-
+                System.out.println();
             }
-            System.out.println();
-        }
-        System.out.println("Plz enter OX coordinate: ");
-        int x = scanner.nextInt();
-        System.out.println("Plz enter OY coordinate: ");
-        int y = scanner.nextInt();
-        if (battlefield[x][y] == 1) {
-            System.out.println("Hit!");
-        } else {
-
+            System.out.println("Plz enter OX coordinate: ");
+            int x = scanner.nextInt();
+            System.out.println("Plz enter OY coordinate: ");
+            int y = scanner.nextInt();
+            if (battlefield[x][y] == 1) {
+                System.out.println("Hit!");
+                monitor[x][y] = 2;
+            } else {
+                System.out.println("Miss!");
+                monitor[x][y] = 1;
+                break;
+            }
         }
 
     }
-}
+
+    public static boolean isWinCondition() {
+        int counter1 = 0;
+        for (int i = 0; i < monitor1.length; i++) {
+            for (int j = 0; j < monitor1[i].length; j++) {
+                if (monitor1[i][j] == 2) {
+                    counter1++;
+                }
+            }
+        }
+
+        int counter2 = 0;
+        for (int i = 0; i < monitor2.length; i++) {
+            for (int j = 0; j < monitor2[i].length; j++) {
+                if (monitor2[i][j] == 2) {
+                    counter2++;
+                }
+
+
+            }
+        }
+
+        if (counter1 >= 10) {
+            System.out.println(playerName1 + " win!");
+            return true;
+        }
+        if (counter2 >= 10) {
+            System.out.println(playerName2 + " win!");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isAvailable(int x, int y, int deck, int rotation, int[][] battlefield) {
+        if (rotation == 1) {
+            if (y + deck > battlefield.length) {
+                return false;
+            }
+        }
+        if (rotation == 2) {
+            if (x + deck > battlefield[0].length) {
+                return false;
+            }
+        }
+        while (deck!=0){
+            for (int i = 0; i < deck; i++) {
+                int xi = 0;
+                int yi = 0;
+                if (rotation == 1){
+                    yi = i;
+                }else {
+                    xi =i;
+                }
+                battlefield [x][y];
+                if (x + 1 + xi < battlefield.length && x + 1 + xi >=0){
+                    if (battlefield[x + 1 + xi ][y + yi]!=0){
+                        return false;
+                    }
+                }if (x - 1 + xi < battlefield.length && x - 1 + xi >=0){
+                    if (battlefield[x - 1 + xi ][y + yi]!=0){
+                        return false;
+                    }
+                }if (y+ 1 + yi < battlefield.length && y + 1 + yi >=0){
+                    if (battlefield[x+ xi ][y +1 + yi]!=0){
+                        return false;
+                    }
+                }if (y - 1 + yi < battlefield.length && y - 1 + yi >=0){
+                    if (battlefield[x + xi ][y -1 + yi]!=0){
+                        return false;
+                    }
+                }
+            }deck--;
+        }return true;
+
+    }}
 
 
